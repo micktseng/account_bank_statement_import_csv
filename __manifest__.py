af216de0ae8c0530e85e0deb834b8718369b2f03
Mick <suncombo@gmail.com>
2017-04-25 15:31:31 +0800

    [FIX]修改描述

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,10 @@
 # -*- encoding: utf-8 -*-
-# © 2016 Samuel Lefever, Jerome Sonnet
-# © 2016 Niboo SPRL (<https://www.niboo.be/>), Be-Cloud
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Bank Statements CSV Import',
     'category': 'Accounting & Finance',
-    'summary': 'Import Bank Statements from Luxembourg',
+    'summary': 'Import Bank Statements from CSV',
     'website': '',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
